91f43fedd1fcddcc1f1b6bc92829db44a45acf00
unknown <anna.sh.pb@gmail.com>
2023-07-17 23:12:07 +0300

    add some content to the home page

--- src/layouts/Home.tsx
+++ src/layouts/Home.tsx
@@ -1,1 +1,12 @@
-export const Home = () => <h1>Welcome to Hogwarts!</h1>;
+export const Home = () => (
+	<>
+		<h1>Welcome to Hogwarts!</h1>
+		<p>
+			Hello, our dear friends! Let us introduce our{' '}
+			<strong>Hogwarts School of Witchcraft and Wizardry</strong>! It is a
+			school of magic for witches and wizards between the ages of eleven and
+			seventeen living in the United Kingdom, the Republic of Ireland and
+			Scotland.
+		</p>
+	</>
+);
