6b4c592b5b2b6d52392a7acf41050404520c1645
unknown <anna.sh.pb@gmail.com>
2023-07-19 13:33:36 +0300

    add the back link to the single staff member page

--- src/layouts/SingleStaffMember.tsx
+++ src/layouts/SingleStaffMember.tsx
@@ -1,7 +1,12 @@
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 
 export const SingleStaffMember = () => {
 	const { id } = useParams();
 
-	return <h1>A staff member #{id} will teach your child how to stay safe</h1>;
+	return (
+		<>
+			<h1>A staff member #{id} will teach your child how to stay safe</h1>
+			<Link to='../'>Back to all the staff</Link>
+		</>
+	);
 };
