622b7381a54a3d3b16299fff6fa553e6b47100e1
unknown <anna.sh.pb@gmail.com>
2023-07-25 23:57:07 +0300

    fetch the students data to the single house page

--- src/pages/SingleHouse.tsx
+++ src/pages/SingleHouse.tsx
@@ -1,9 +1,67 @@
+import { useState, useEffect, useCallback } from 'react';
 import { useParams } from 'react-router-dom';
+import { CircularProgress, Grid } from '@mui/material';
+
+interface IHouseStudent {
+	name: string;
+}
 
 export const SingleHouse = () => {
 	const { name } = useParams();
+	const [students, setStudents] = useState([]);
+	const [loading, setLoading] = useState(true);
+	const [errorMessage, setErrorMessage] = useState(null);
+
+	const fetchHouseStudents = useCallback(() => {
+		fetch(`https://hp-api.onrender.com/api/characters/house/${name}`)
+			.then((res) => {
+				if (res.ok) {
+					return res.json();
+				} else {
+					throw new Error('Oooooops, Muggle-Repelling charm in action');
+				}
+			})
+			.then((data) => {
+				if (data.length > 0) {
+					setStudents(data);
+					setLoading(false);
+				} else {
+					throw new Error(
+						"I'm afraid, there's no such house in Hogwarts, honey"
+					);
+				}
+			})
+			.catch((err) => {
+				setLoading(false);
+				setErrorMessage(err.message);
+			});
+	}, [name]);
+
+	useEffect(() => {
+		if (!loading) setLoading(true);
+		if (errorMessage) setErrorMessage(null);
+		fetchHouseStudents();
+	}, [fetchHouseStudents]);
 
 	return (
-		<h2>Welcome to {name && name.charAt(0).toUpperCase() + name.slice(1)}</h2>
+		<>
+			<h2>Welcome to {name && name.charAt(0).toUpperCase() + name.slice(1)}</h2>
+			{loading ? (
+				<CircularProgress />
+			) : errorMessage ? (
+				<p>{errorMessage}</p>
+			) : (
+				students &&
+				students.length > 0 && (
+					<Grid container spacing={2}>
+						{students.map((student: IHouseStudent) => (
+							<Grid item xs={12} md={6} lg={4}>
+								{student.name}
+							</Grid>
+						))}
+					</Grid>
+				)
+			)}
+		</>
 	);
 };
